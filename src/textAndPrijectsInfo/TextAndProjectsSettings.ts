99b707394289aab6b064591c82f1b5388dd2fa0d
Harbatau <npikolist@gmail.com>
2020-09-16 20:28:17 +0300

    text editing

--- src/textAndPrijectsInfo/TextAndProjectsSettings.ts
+++ src/textAndPrijectsInfo/TextAndProjectsSettings.ts
@@ -27,11 +27,11 @@ export const projectsInfo: ProjectType[] = [
     {
         projectName: "The Counter",
         projectDescription: "My first project",
-        detailedDescription: "The project everyone started with. It doesn't have much logic, but looks not so bad. " +
-            "I think that it is not a shame to show it to someone",
-        pros: "The project is responsive and has a nice appearance. There are two display modes. " +
+        detailedDescription: "This is the project everyone started with. It doesn't have much logic, but doesn't look bad either. " +
+            "I think one shouldn't be ashamed to show it to someone.",
+        pros: "This project is responsive and has a nice appearance. There are two display modes. " +
             "It also has advanced input validation with a set of messages for each error.",
-        cons: "Slight short-term performance degradation may occur when switching modes. The validation logic is quite bulky",
+        cons: "Slight short-term performance degradation may occur when switching the modes. The validation logic is quite bulky",
         technologies: "JavaScript  React  Redux  SASS",
         color: '#847EA0',
         backgroundColor: '#2E2B3B',
@@ -43,9 +43,9 @@ export const projectsInfo: ProjectType[] = [
     {
         projectName: "To Do List",
         projectDescription: "An application with many interactions",
-        detailedDescription: "My second project. Connected to the server API, kindly provided to me by IT-incubator. " +
-            "When developing, the main task was to create a beautiful and responsive application with a huge number of all kinds of user interactions. " +
-            " Design inspired by Apple's Neumorphism.",
+        detailedDescription: "This is my second project. Connected to the server API, kindly provided to me by the IT-incubator. " +
+            "When developing, the main task was to create a beautiful and responsive application with a huge number of various user interactions. " +
+            " Design is inspired by Apple's Neumorphism.",
         pros: "There are animations and transitions for almost any user action. Data will be sent only when the user accepts the changes that can be canceled. " +
             "Changes are made by comparing the original state with the edited one.",
         cons: "Due to the large number of animations and event handlers, performance issues can be observed on slower devices. " +
@@ -61,12 +61,12 @@ export const projectsInfo: ProjectType[] = [
     {
         projectName: "The Portfolio",
         projectDescription: "The project that characterizes me",
-        detailedDescription: "In my opinion, Three.js is a technology that greatly improves perceptions and user experience. " +
-            "At least After I got acquainted with this library, I had no doubts about it. " +
+        detailedDescription: "In my opinion, Three.js is a technology that greatly improves perception and user experience. " +
+            "Personally, after I had gotten acquainted with this library, I didn't have any doubts about it anymore. " +
             "Even if you don't have expensive and beautiful 3d models, you can surprise users with your creativity.",
-        pros: "Main advantage of this project is animated canvas in the background that keeps track" +
-            " of what you are watching and changes accordingly to it. There are also non-standard user interactions here.",
-        cons: "Possible performance degradation on devices with weak 3d accelerator. Also some bugs on mobile devices are possible.",
+        pros: "The main advantage of this project is the animated canvas in the background that keeps track" +
+            " of what one is watching and changes accordingly. There are also non-standard user interactions here.",
+        cons: "Possible performance degradation on devices with a weak 3d accelerator. Also, some bugs on mobile devices are possible.",
         technologies: "TypeScript React Redux styled-components react-spring Three.js",
         color: "#ff0000",
         backgroundColor: '#490000',
